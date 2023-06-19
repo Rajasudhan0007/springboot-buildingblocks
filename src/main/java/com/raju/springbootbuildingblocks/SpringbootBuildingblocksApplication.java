2e3c91171dead0f36c18867f9ae9ec5e6ec1857b
rsa9 <rsa9@US-5CG0253H8F>
2023-06-19 12:19:54 +0530

    Third Commit

--- src/main/java/com/raju/springbootbuildingblocks/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/raju/springbootbuildingblocks/SpringbootBuildingblocksApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //Hello
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
-
+//main application
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
 	}
